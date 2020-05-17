7239c3e6c198637aeb73646fce23547e33ccfa2c
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2020-05-18 00:21:02 +0900

    fix(healthckeck.sh): ignore SC2016

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,6 +2,7 @@
 
 set -eu -o pipefail
 
+# shellcheck disable=SC2016
 echo 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*' \
   | scandata.sh \
   | grep -qE '^DONE OK 0203 Virus found during virus scan'
