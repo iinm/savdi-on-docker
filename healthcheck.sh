523d7f91d17e127db406a19c7fde4779caae4339
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2020-05-18 00:08:43 +0900

    fix(healthcheck): check with test virus

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,4 +2,6 @@
 
 set -eu -o pipefail
 
-echo 'hello' | scandata.sh | grep -qE "^DONE OK 0000"
+echo 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*' \
+  | scandata.sh \
+  | grep -qE '^DONE OK 0203 Virus found during virus scan'
